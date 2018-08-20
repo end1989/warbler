ea542e990b56e68bd139508d4c29369fcca57ca2
Eldon Dahlin <end1989@gmail.com>
2018-08-20 17:24:45 -0600

    Backend finished

---
+++ index.js
@@ -0,0 +1,51 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const errorHandler = require("./handlers/error");
+const authRoutes = require("./routes/auth");
+const messagesRoutes = require("./routes/messages");
+const { loginRequired, ensureCorrectUser } = require("./middleware/auth");
+
+const PORT = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use("/api/auth", authRoutes);
+app.use(
+    "/api/users/:id/messages",
+    loginRequired,
+    ensureCorrectUser,
+    messagesRoutes
+);
+
+app.get("/api/message", loginRequired, async function(req, res, next) {
+    try {
+        let messages = await db.Message.find()
+            .sort({
+                createdAt: "desc"
+            })
+            .populate("user", {
+                username: true,
+                profileImage: true
+            });
+        return res.status(200).json(messages);
+    } catch (err) {
+        return next(err);
+    }
+});
+
+app.use((req, res, next) => {
+    let err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+});
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+    console.log("Server is starting on port ", PORT);
+});
